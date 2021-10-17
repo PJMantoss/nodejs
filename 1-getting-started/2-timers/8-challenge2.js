1f4d9a6f6f7bc0e4f763f3878a2c1b7b78694387
PJMantoss <joelptoss@gmail.com>
2021-10-17 22:31:11 +0100

    challenge 8

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,14 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+for(let i=0; i<=5; i++){
+    let count = 0;
+    if(i < 5){
+        count++;
+        console.log('Hello World');
+    }else if (i === 5){
+        console.log('Done')
+        break;
+    }
+}
